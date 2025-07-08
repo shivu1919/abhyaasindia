e14cbf93c60fb64f7f6ceb971677036611024f8e
shivu1919 <shivamthakur.ss129@gmail.com>
2025-07-08 15:48:03 +0530

    second commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from "react-router-dom"
 import './Header.css'
 
 function Header() {
+
+
+    const[screen,setScreen] = useState('large')
+    const[menIcon, setMenuIcon] = useState('/icons/menu-bar.png')
+
   return (
     <>
         
@@ -25,9 +30,37 @@ function Header() {
                     </div>
                 </div>
 
-                <div id="header-menu-bar">
-                    <img src="/icons/menu-bar.png" width="40"/>
+                <div id="header-menu-bar" onClick={()=>{
+                    if(screen=='large'){
+                        setScreen('small')
+                        setMenuIcon('/icons/close.png')
+                    }
+
+                    else{
+                        setScreen('large')
+                        setMenuIcon("/icons/menu-bar.png")
+                    }
+                }}>
+                    <img src={menIcon} width="40"/>
                 </div>
+            </div>
+
+            <br/><br/><br/>
+
+            <div id={screen}>
+                    
+                        <Link id="header-links" to="/">Home</Link>
+                        <Link id="header-links" to="/">Services</Link>
+                        <Link id="header-links" to="/">About</Link>
+                        <Link id="header-links" to="/">Contact</Link>
+
+                    <div style={{backgroundColor:"red",padding:"6px",borderRadius:"12px",cursor:"pointer"}}>
+                        <div style={{display:"flex",alignItems:"center",columnGap:"5px"}}>
+                            <img src="/icons/phone-call.png" width="30"/>
+                            <p style={{fontSize:"18px",color:"white",fontWeight:"500"}}>8630444460</p>
+                        </div>
+                    </div> 
+                    
             </div>
         
     </>
