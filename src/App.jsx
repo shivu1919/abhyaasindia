cd743848611d9e9bdcf9dfc0d82ab759b3305ccb
shivu1919 <shivamthakur.ss129@gmail.com>
2025-07-10 14:53:49 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Header from './components/Header'
 import { BrowserRouter } from 'react-router-dom'
+import Home from './pages/Home'
 
 function App() {
   return (
     <>
     <BrowserRouter>
-      <Header/>
+      <Home/>
     </BrowserRouter>
         
     </>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../components/Header'
+import '../css/Home.css'
+
+function Home() {
+  return (
+    <>
+        <Header/>
+        <div id='home-first'>
+                <h1 id='heading1'>Welcome To</h1>
+                <h1 id='heading2'>AbhyaasIndia</h1>
+        </div>
+    </>
+  )
+}
+
+export default Home
